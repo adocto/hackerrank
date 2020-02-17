d7a9e756473620354df2bdf3c71cc0421fbe9859
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-17 16:52:08 -0500

    Update stringsplitter.py

--- mystuff/stringsplitter.py
+++ mystuff/stringsplitter.py
@@ -1,6 +1,8 @@
 def split_and_join(line):
     # write your code here
-
+    newLine = line.split(" ")
+    result = "-".join(newLine)
+    return result
 
 if __name__ == '__main__':
     line = input()
