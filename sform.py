87c4846c2ab1b14205a28076cb27a24f28ba7763
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-20 14:23:54 -0500

    Update sform.py

--- sform.py
+++ sform.py
@@ -1,6 +1,9 @@
 def print_formatted(number):
     # your code goes here
-    
+    i = 1
+    number = number
+    for i in range(number):
+        print("%d" % number + "%s" %(number," ") + "%d" % oct(number) + "%s" %(number," ") + "%d" %hex(number).capitalize() + "%d" % bin(number))
 
 if __name__ == '__main__':
     n = int(input())
