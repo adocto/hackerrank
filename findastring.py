0c5c1ab82f0e6bce20cbc09e9b86eabcd2f6a1c0
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-20 23:23:34 -0500

    Update findastring.py

--- findastring.py
+++ findastring.py
@@ -1,6 +1,18 @@
 def count_substring(string, sub_string):
-    
-    return
+    i = 0
+    count = 0
+
+    for i in range(len(string)-1):
+
+        if string[i] == sub_string[0]:
+            if string[i:(i + len(sub_string))] == sub_string:
+                count += 1
+        i +=1
+
+
+
+
+    return count
 
 
 if __name__ == '__main__':
